c384047a83bc2db6dd711db0c1d7770a825c79f3
bluemmb <bluemmb22@gmail.com>
2017-09-20 15:52:57 +0000

    Initialize Controller: Dashboard

---
+++ app/Http/Controllers/Dashboard/View.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+
+class View extends Controller
+{
+    public function index(){
+        return "This is Dashboard Page.";
+    }
+
+}
