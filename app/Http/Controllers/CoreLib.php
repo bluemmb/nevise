6249494e4756b0b00b88dda97f336bb9ce12563c
bluemmb <bluemmb22@gmail.com>
2017-09-21 14:17:41 +0000

    Update CoreLib: Add getProjectDetails()

--- app/Http/Controllers/CoreLib.php
+++ app/Http/Controllers/CoreLib.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class CoreLib
 {
+    public function getProjectDetails($id)
+    {
+        $project_details = DB::select( "select * from projects where id = ? limit 1" , [ $id ] );
 
+        if ( count( $project_details ) == 0 )
+            return null;
+
+        return $project_details[0];
+    }
 }
