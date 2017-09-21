78a38d4e43046a958097f561080bab1c1a05af33
sinamcr7 <sinamcr7@gmail.com>
2017-09-21 07:50:02 +0000

    Add Route: Project

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get( "dashboard" , "Dashboard\View@index" );
+Route::get('project/{id}','Project\View@index');
 
