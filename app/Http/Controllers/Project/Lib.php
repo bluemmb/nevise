8f95b270b04cbf9597b0e99341d748e071579969
sinamcr7 <sinamcr7@gmail.com>
2017-09-21 07:56:00 +0000

    Add Controller: Project

---
+++ app/Http/Controllers/Project/Lib.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Project;
+
+use App\Http\Controllers\CoreLib;
+use Illuminate\Database;
+
+class Lib extends CoreLib
+{
+    public function SelectAll()
+    {
+        
+    }
+}
---
+++ app/Http/Controllers/Project/View.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Project;
+
+use App\Http\Controllers\Controller;
+
+use App\Http\Controllers\Project\Lib as Project_Lib;
+
+class View extends Controller
+{
+    public $lib;
+
+    function _construct()
+    {
+        $this->lib=new Project_Lib();
+    }
+
+    public function index()
+    {
+        
+    }
+}
