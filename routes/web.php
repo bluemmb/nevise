c00d37122c7b331c0b51d30a9b424fefb4a98641
bluemmb <bluemmb22@gmail.com>
2017-09-23 11:44:20 +0000

    Route Change: home to dashboard

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Auth::routes();
 
 
 // --- Home Routes
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::get( "home" , "Dashboard\View@index" );
 
 
 // --- Dashboard Routes
